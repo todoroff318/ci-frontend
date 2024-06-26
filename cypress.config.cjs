dd5f8bfbcd2f537c2e091f80402ae2ba6a346a54
todoroff318 <niki336699@gmail.com>
2024-06-26 20:44:31 +0300

    Update cypress.config.cjs

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
   e2e: {
-    baseUrl: 'http://localhost:8081', // Ensure this matches your application URL
+    baseUrl: 'http://localhost:8080', // Ensure this matches your application URL
     supportFile: 'cypress/support/index.js',
     fixturesFolder: 'cypress/fixtures',
     specPattern: 'cypress/e2e/**/*.cy.js', // Update this pattern to match your test file naming convention
